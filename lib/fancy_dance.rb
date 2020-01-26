0527d252a9e4b93a74c9f7081d4ff7bc37244515
Hannah Harrington <hannahharrington@outlook.com>
2020-01-26 17:10:05 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,15 +1,27 @@
-require_relative './fancy_dance.rb'
+module FancyDance
+  module InstanceMethods
 
+    def twirl
+      "I'm twirling!"
+    end
 
-class Dancer
-  
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-    
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
+    def jump
+      "Look how high I'm jumping!"
+    end
+
+    def pirouette
+      "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
   end
-  
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
 end
